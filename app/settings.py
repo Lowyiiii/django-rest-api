7257f0543f1f70cde159bb0f0a35b2795311d36b
lowyiiii <mr.lowbattery@gmail.com>
2020-02-08 20:34:52 +0330

    Final commit

--- app/settings.py
+++ app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'users',
+    'recipe',
 ]
 
 MIDDLEWARE = [
@@ -123,4 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = '/home/master/Desktop/'
+STATIC_ROOT = '/home/master/Desktop'
 AUTH_USER_MODEL = 'core.User'
